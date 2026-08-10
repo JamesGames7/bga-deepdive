3c6ba2b8e42583716e185498b7e5827a4e30f156
Connor <connor@srask.ca>
2026-08-10 18:12:41 -0400

    initial commit

---
+++ src/ts/types.d.ts
@@ -0,0 +1,18 @@
+interface DeepDivePlayer extends Player {
+    energy: number; // any information you add on each result['players']
+}
+
+interface DeepDiveGamedatas extends Gamedatas<DeepDivePlayer> {
+    // Add here variables you set up in getAllDatas
+}
+   
+/*
+ * Describe here the types for your state args
+ */
+interface PlayerTurnArgs {
+    playableCardsIds: number[];
+}
+   
+/*
+ * Describe here the types for your notif args
+ */
